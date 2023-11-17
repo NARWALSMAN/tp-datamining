1ff36559982b1e705c7baf855d7cf07fced5a21b
theo guesdon <55153254+NARWALSMAN@users.noreply.github.com>
2023-11-17 11:20:13 +0100

    Update tp.R

--- tp.R
+++ tp.R
@@ -29,21 +29,44 @@ data_iris<-iris
 data_train_versicolor <- subset(data_iris, Species == "versicolor")[1:25, ]
 data_train_virginica<-subset(data_iris, Species == "virginica")[1:25, ]
 data_train_setosa<-subset(data_iris, Species == "setosa")[1:25, ]
-data_train<-rbind(data_train_setosa,data_train_versicolor,data_train_virginica)
+data_train<-rbind(data_train_setosa,data_train_versicolor,data_train_virginica)[,1:4]
 label_train<-c(rep("setosa",25),rep("versicolor",25),rep("virginica",25))
 #test
 data_test_versicolor <- subset(data_iris, Species == "versicolor")[26:50, ]
 data_test_virginica<-subset(data_iris, Species == "virginica")[26:50, ]
 data_test_setosa<-subset(data_iris, Species == "setosa")[26:50, ]
-data_test<-rbind(data_test_setosa,data_test_versicolor,data_test_virginica)
+data_test<-rbind(data_test_setosa,data_test_versicolor,data_test_virginica)[,1:4]
 label_test<-c(rep("setosa",25),rep("versicolor",25),rep("virginica",25))
 
-test_resultat<-knn(train = data_train[,1:4], test = data_test[,1:4],cl = label_train,k = 3,use.all = FALSE)
+test_resultat<-knn(train = data_train, test = data_test,cl = label_train,k = 3,use.all = FALSE,set.seed(4))
 test_resultat
 
-table(test_resultat,data_test[,5])
+tableau_confusion<-table(label_test,test_resultat)
+tauxerreur<-(tableau_confusion[1,2]+tableau_confusion[1,3]+tableau_confusion[2,3]+
+  tableau_confusion[2,1]+tableau_confusion[3,1]+tableau_confusion[3,2])/75
+precision<-(tableau_confusion[1,1]+tableau_confusion[2,2]+tableau_confusion[3,3])/
+  (tableau_confusion[1,2]+tableau_confusion[1,3]+tableau_confusion[2,3]+
+     tableau_confusion[2,1]+tableau_confusion[3,1]+tableau_confusion[3,2]+
+     tableau_confusion[1,1]+tableau_confusion[2,2]+tableau_confusion[3,3])
+tauxerreur
+precision
 #QUESTION 3  Programmer une fonction 
-
+fait_un_test<-function(donnertrain,donnertest,classetrain,classetest,k){
+  #creer un test de knn et test avec une matrice de confusion la precision
+  #entrée: donnertrain :  dataframe  donner de training du modele
+  #        donnertest:    dataframe  donner a tester
+  #        classetrain:   liste      mliste des classe du datatrain
+  #        classetest:    liste      liste des classe du donnertest
+  #        k: integer     k          voisin 
+  #sortie: precision:     intezger   renvooi la precision calculer du test
+  test_resultat_1<-knn(train = donnertrain, test = donnertest,cl = classetrain,k = k,use.all = FALSE,set.seed(4))
+  tableau_confusion_1<-table(classetest,test_resultat_1)
+  precision_1<-(tableau_confusion_1[1,1]+tableau_confusion_1[2,2]+tableau_confusion_1[3,3])/
+    (tableau_confusion_1[1,2]+tableau_confusion_1[1,3]+tableau_confusion_1[2,3]+
+       tableau_confusion_1[2,1]+tableau_confusion_1[3,1]+tableau_confusion_1[3,2]+
+       tableau_confusion_1[1,1]+tableau_confusion_1[2,2]+tableau_confusion_1[3,3])
+  return(precision_1)
+}
 
 #QUESTION 4  Tester votre fonction sur le jeu entier
 
